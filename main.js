abe9c5a1c7ffac5bfd812164798c4266b6000aa1
なりたけいすけ <naritakeisuke@team-lab.com>
2019-11-23 22:18:03 +0900

    充電情報取得

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const { app } = require('electron');
+const { execSync } = require('child_process');
+
+
+
+const getChargerInfo = () => {
+  const stdout = execSync('ioreg -rn AppleSmartBattery | grep \\\"AdapterDetails\\\"');
+  const res = stdout.toString().match(/\{.+\}/)[0];
+  const info = JSON.parse(res.replace(/=/g, ':'));
+  return info;
+};
+
 
 app.on('ready', () => {
   console.log('hello!');
