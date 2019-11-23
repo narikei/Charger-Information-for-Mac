06faaf5efe4cbb4cfe9db5566b7603e2d79f3a10
なりたけいすけ <naritakeisuke@team-lab.com>
2019-11-23 22:32:17 +0900

    充電情報を表示

--- main.js
+++ main.js
@@ -7,15 +7,32 @@ const ICON_PATH = `${__dirname}/images/icon.png`;
 
 let appIcon;
 let menu
+let menuWatts;
+let menuVoltage;
+let menuCurrent;
 
 
 const initMenu = () => {
   appIcon = new Tray(ICON_PATH);
+
+  menuWatts = new MenuItem({
+    enabled: false,
+  });
+  menuVoltage = new MenuItem({
+    enabled: false,
+  });
+  menuCurrent = new MenuItem({
+    enabled: false,
+  });
 };
 
 const updateMenu = () => {
   menu = new Menu();
 
+  menu.append(menuWatts);
+  menu.append(menuVoltage);
+  menu.append(menuCurrent);
+  menu.append(new MenuItem({ type: 'separator' }));
   menu.append(new MenuItem({ role: 'quit' }));
 
   appIcon.setContextMenu(menu);
@@ -36,6 +53,10 @@ app.on('ready', () => {
 
   const chargerInfo = getChargerInfo();
   appIcon.setTitle(`${chargerInfo.Watts}W`);
+
+  menuWatts.label = `Watts: ${chargerInfo.Watts}W`;
+  menuVoltage.label = `Voltage: ${chargerInfo.Voltage / 1000}V`;
+  menuCurrent.label = `Current: ${chargerInfo.Current / 1000}A`;
 
   updateMenu();
 });
