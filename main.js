4417b56d703a615922738e87629bf5d267d6534a
なりたけいすけ <naritakeisuke@team-lab.com>
2019-11-23 22:30:22 +0900

    メニュー追加

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, Tray } = require('electron');
+const { app, Tray, Menu, MenuItem } = require('electron');
 const { execSync } = require('child_process');
 
 
@@ -6,10 +6,19 @@ const ICON_PATH = `${__dirname}/images/icon.png`;
 
 
 let appIcon;
+let menu
 
 
 const initMenu = () => {
   appIcon = new Tray(ICON_PATH);
+};
+
+const updateMenu = () => {
+  menu = new Menu();
+
+  menu.append(new MenuItem({ role: 'quit' }));
+
+  appIcon.setContextMenu(menu);
 };
 
 
@@ -27,4 +36,6 @@ app.on('ready', () => {
 
   const chargerInfo = getChargerInfo();
   appIcon.setTitle(`${chargerInfo.Watts}W`);
+
+  updateMenu();
 });
